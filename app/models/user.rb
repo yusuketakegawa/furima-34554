cc0e660c183993c980539cede6809c3bb9a6c940
yusuketakegawa <zyou06979@gmail.com>
2021-02-17 18:14:12 +0900

    validationの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,19 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, precense: true
-  validates :emali,    precense: true
-  validates :email,    unique:  true
-  validates :email, precense: true
-  validates :email, inclusion: {in: %w(@) }
-  validates :encrypted_password, precense: true
-  validates :encrypted_password, length: {minimum: 6 }
+  validates :nickname,        presence: true
+  validates :last_name,       presence: true
+  validates :first_name,      presence: true
+  validates :last_name_kana,  presence: true
+  validates :fitst_name_kana, presence: true
   validates :encrypted_password, format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/}
-  validates :password_confirmation, password:true
-  validates :last_name,     precense: true
-  validates :first_name,    precense: true
-  validates :last_name_kana,precense: true
-  validates :fitst_name_kana,precense: true
-
-
+  validates :encrypted_password, confirmation: true
 end
