d2e399630d5c487b409ff54a50bb7005046dd091
yusuketakegawa <zyou06979@gmail.com>
2021-02-22 12:36:42 +0900

    モデルの単体コード作成しました。

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
-    
+    name             {'ゴミ箱'}
+    item_explanation {'いいね'}
+    price            {'1000'}
+    category_id      {'2'}
+    status_id        {'2'}
+    delivery_fee_id  {'2'}
+    prefecture_id    {'2'}
+    shipping_date_id   {'2'}
+
+    association :user
+
+    after(:build) do |message|
+      message.image.attach(io: File.open('public/images/test.image.png'), filename: 'test.image.png')
+    end
   end
 end
