da89849663cf78973bef1746c994044b13b11549
yusuketakegawa <zyou06979@gmail.com>
2021-02-23 12:36:54 +0900

    ログイン状態の出品者のみ商品情報を削除できる。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,6 +23,12 @@ class ItemsController < ApplicationController
       @item = Item.find(params[:id])
     end
 
+    def destroy
+      item = Item.find(params[:id])
+      item.destroy
+      redirect_to root_path
+    end
+
   private
   def item_params
     params.require(:item).permit(:name, :item_explanation, :price, :prefecture_id,:shipping_date_id,:status_id,:category_id,:delivery_fee_id, :image).merge(user_id: current_user.id)
