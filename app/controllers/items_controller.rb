a2e7b5bbacceb4f3cd8eb13e733907009d8b5644
yusuketakegawa <zyou06979@gmail.com>
2021-02-23 14:40:41 +0900

    変更箇所を修正しました。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,7 +25,7 @@ class ItemsController < ApplicationController
 
     def destroy
       item = Item.find(params[:id])
-       if current_user.id
+       if current_user.id == item.user_id
       item.destroy
       redirect_to root_path
        end
