ba129ea2daef2f263525a11421d6c5423dfc2f18
yusuketakegawa <zyou06979@gmail.com>
2021-03-01 17:50:34 +0900

    変更箇所を修正しました。

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -40,6 +40,6 @@ class BuysController < ApplicationController
   end
 
   def set_item2
-    redirect_to root_path unless current_user.id == @item.buy.id
+    redirect_to root_path if current_user.id || @item.buy.presence
   end
 end
